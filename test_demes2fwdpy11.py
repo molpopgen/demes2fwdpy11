1739d53d7b2805248dc4264ffe490ded313a3af3
Aaron Ragsdale <aaron.ragsdale@mail.mcgill.ca>
2020-11-06 14:14:46 -0600

    decorate tests

--- test_demes2fwdpy11.py
+++ test_demes2fwdpy11.py
@@ -5,26 +5,36 @@ import unittest
 import fwdpy11
 
 
-class TestSingleDeme(unittest.TestCase):
-    @classmethod
-    def setUp(self):
-        self.g = demes.DemeGraph(
-            description="test demography", time_units="generations"
-        )
-
-    def valid_fwdpy11_demography(self):
-        self.g.get_demographic_events()
-        self.demog = build_model.build_from_deme_graph(self.g, 10)
+def check_valid_demography(cls):
+    def _valid_fwdpy11_demography(self):
         try:
             _ = fwdpy11.DemographyDebugger([100], self.demog)
         except:
             self.fail("unexpected exception")
 
-    def test_constant_size(self):
-        self.g.deme(id="deme", initial_size=1000)
-        self.valid_fwdpy11_demography()
+    cls.test_validity = _valid_fwdpy11_demography
+    return cls
 
-    def test_two_epoch_model(self):
+
+@check_valid_demography
+class TestNoEvents(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(
+            description="test demography", time_units="generations"
+        )
+        self.g.deme(id="deme", initial_size=1000)
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+
+@check_valid_demography
+class TestTwoEpoch(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(
+            description="test demography", time_units="generations"
+        )
         self.g.deme(
             id="deme",
             epochs=[
@@ -32,7 +42,10 @@ class TestSingleDeme(unittest.TestCase):
                 demes.Epoch(initial_size=2000, end_time=0),
             ],
         )
-        self.valid_fwdpy11_demography()
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+    def test_size_change_params(self):
         self.assertTrue(len(self.demog.model.set_deme_sizes) == 1)
         self.assertTrue(
             self.demog.model.set_deme_sizes[0].when
@@ -41,22 +54,13 @@ class TestSingleDeme(unittest.TestCase):
         self.assertTrue(self.demog.model.set_deme_sizes[0].new_size == 2000)
 
 
+@check_valid_demography
 class TestNonGenerationUnits(unittest.TestCase):
     @classmethod
-    def setUp(self):
+    def setUpClass(self):
         self.g = demes.DemeGraph(
             description="test demography", time_units="years", generation_time=25
         )
-
-    def valid_fwdpy11_demography(self):
-        self.g.get_demographic_events()
-        self.demog = build_model.build_from_deme_graph(self.g, 10)
-        try:
-            _ = fwdpy11.DemographyDebugger([100], self.demog)
-        except:
-            self.fail("unexpected exception")
-
-    def test_single_deme_years(self):
         self.g.deme(
             id="Pop",
             epochs=[
@@ -64,7 +68,10 @@ class TestNonGenerationUnits(unittest.TestCase):
                 demes.Epoch(initial_size=1000, final_size=20000, end_time=0),
             ],
         )
-        self.valid_fwdpy11_demography()
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+    def test_conversion_to_generations(self):
         self.assertTrue(
             self.demog.model.set_deme_sizes[0].when
             == self.demog.metadata["burnin_time"]
@@ -76,22 +83,13 @@ class TestNonGenerationUnits(unittest.TestCase):
         self.assertTrue(self.demog.model.set_deme_sizes[0].new_size == 1000)
 
 
-class TestSelfing(unittest.TestCase):
+@check_valid_demography
+class TestSelfingShift(unittest.TestCase):
     @classmethod
-    def setUp(self):
+    def setUpClass(self):
         self.g = demes.DemeGraph(
             description="test demography", time_units="generations"
         )
-
-    def valid_fwdpy11_demography(self):
-        self.g.get_demographic_events()
-        self.demog = build_model.build_from_deme_graph(self.g, 10)
-        try:
-            _ = fwdpy11.DemographyDebugger([100], self.demog)
-        except:
-            self.fail("unexpected exception")
-
-    def test_single_pop_selfing_shift(self):
         self.g.deme(
             id="Selfer",
             epochs=[
@@ -99,70 +97,123 @@ class TestSelfing(unittest.TestCase):
                 demes.Epoch(initial_size=1000, end_time=0, selfing_rate=0.2),
             ],
         )
-        self.valid_fwdpy11_demography()
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+    def test_selfing_parameters(self):
         self.assertTrue(
             self.demog.model.set_selfing_rates[0].when
             == self.demog.metadata["burnin_time"]
         )
         self.assertTrue(self.demog.model.set_selfing_rates[0].S == 0.2)
 
-    def test_single_pop_selfing(self):
+
+@check_valid_demography
+class TestSelfing(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(
+            description="test demography", time_units="generations"
+        )
         self.g.deme(id="Selfer", initial_size=1000, selfing_rate=0.5)
-        self.valid_fwdpy11_demography()
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+    def test_single_pop_selfing(self):
         self.assertTrue(self.demog.model.set_selfing_rates[0].when == 0)
         self.assertTrue(self.demog.model.set_selfing_rates[0].S == 0.5)
 
 
-class TestSplits(unittest.TestCase):
+@check_valid_demography
+class TestSplit(unittest.TestCase):
     @classmethod
-    def setUp(self):
+    def setUpClass(self):
         self.g = demes.DemeGraph(
             description="test demography", time_units="generations"
         )
         self.g.deme(id="Ancestor", initial_size=1000, end_time=200)
         self.g.deme("Deme1", initial_size=100, ancestors=["Ancestor"])
         self.g.deme("Deme2", initial_size=100, ancestors=["Ancestor"])
-
-    def valid_fwdpy11_demography(self):
         self.g.get_demographic_events()
         self.demog = build_model.build_from_deme_graph(self.g, 10)
-        try:
-            _ = fwdpy11.DemographyDebugger([100], self.demog)
-        except:
-            self.fail("unexpected exception")
 
-    def test_split_no_migration(self):
-        self.valid_fwdpy11_demography()
 
-    def test_split_with_migration(self):
+@check_valid_demography
+class TestSplitMigration(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(
+            description="test demography", time_units="generations"
+        )
+        self.g.deme(id="Ancestor", initial_size=1000, end_time=200)
+        self.g.deme("Deme1", initial_size=100, ancestors=["Ancestor"])
+        self.g.deme("Deme2", initial_size=100, ancestors=["Ancestor"])
         self.g.migration(source="Deme1", dest="Deme2", rate=0.01)
         self.g.migration(source="Deme2", dest="Deme1", rate=0.01)
-        self.valid_fwdpy11_demography()
-
-    def test_three_way_split(self):
-        self.g.deme("Deme3", initial_size=200, ancestors=["Ancestor"])
-        self.valid_fwdpy11_demography()
-
-
-class TestBranches(unittest.TestCase):
-    @classmethod
-    def setUp(self):
-        self.g = demes.DemeGraph(description="test split", time_units="generations")
-        self.g.deme(id="Ancestor", initial_size=1000)
-        self.g.deme("Deme1", initial_size=100, ancestors=["Ancestor"], start_time=100)
-
-    def valid_fwdpy11_demography(self):
         self.g.get_demographic_events()
         self.demog = build_model.build_from_deme_graph(self.g, 10)
-        try:
-            _ = fwdpy11.DemographyDebugger([100], self.demog)
-        except:
-            self.fail("unexpected exception")
 
-    def test_branch(self):
-        self.valid_fwdpy11_demography()
 
-    def test_branch_with_migration(self):
+@check_valid_demography
+class TestSplitThreeWay(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(
+            description="test demography", time_units="generations"
+        )
+        self.g.deme(id="Ancestor", initial_size=1000, end_time=200)
+        self.g.deme("Deme1", initial_size=100, ancestors=["Ancestor"])
+        self.g.deme("Deme2", initial_size=100, ancestors=["Ancestor"])
+        self.g.deme("Deme3", initial_size=200, ancestors=["Ancestor"])
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+
+@check_valid_demography
+class TestBranch(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(description="test branch", time_units="generations")
+        self.g.deme(id="Ancestor", initial_size=1000)
+        self.g.deme("Deme1", initial_size=100, ancestors=["Ancestor"], start_time=100)
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+
+@check_valid_demography
+class TestBranchMigration(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(description="test branch", time_units="generations")
+        self.g.deme(id="Ancestor", initial_size=1000)
+        self.g.deme("Deme1", initial_size=100, ancestors=["Ancestor"], start_time=100)
         self.g.migration(source="Ancestor", dest="Deme1", rate=0.01)
         self.g.migration(source="Deme1", dest="Ancestor", rate=0.01)
-        self.valid_fwdpy11_demography()
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+
+@check_valid_demography
+class TestMultipleBranches(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(description="test branch", time_units="generations")
+        self.g.deme(id="Ancestor", initial_size=1000)
+        self.g.deme("Deme1", initial_size=100, ancestors=["Ancestor"], start_time=100)
+        self.g.deme("Deme2", initial_size=200, ancestors=["Ancestor"], start_time=50)
+        self.g.deme("Deme3", initial_size=300, ancestors=["Deme1"], start_time=20)
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
+
+
+@check_valid_demography
+class TestSplitsBranches(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.g = demes.DemeGraph(description="test", time_units="generations")
+        self.g.deme(id="A", initial_size=1000, end_time=100)
+        self.g.deme(id="B", initial_size=1000, ancestors=["A"], start_time=200)
+        self.g.deme(id="C", initial_size=1000, ancestors=["A"])
+        self.g.deme(id="D", initial_size=1000, ancestors=["A"])
+        self.g.get_demographic_events()
+        self.demog = build_model.build_from_deme_graph(self.g, 10)
